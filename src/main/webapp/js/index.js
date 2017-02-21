208df87bcae5eb4616c4e06fe988a77f3cffed66
sjm690317 <sjm690317@vip.qq.com>
2017-02-21 18:56:16 +0800

    Add files via upload

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -34,4 +34,31 @@ $(".playlist-cover").hover(
   }
 );
 
+$( ".js_left" ).click(function() {
+   var left = $(".playlist-list").css('left').split("px")[0];
+  
+  if(left==0)
+  {
+      $(".playlist-list").animate({left:-2400},600);
+      return 0;
+  }
+  left=parseInt(left)+1200;
+ 
+  $(".playlist-list").animate({left:left},400);
+ 
+});
+
+
+$( ".js_right" ).click(function() {
+  var left = $(".playlist-list").css('left').split("px")[0];
+  left=left-1200;
+  
+  if(left=="-3600")
+  {
+      $(".playlist-list").animate({left:0},600);
+      return 0;
+  }
+  $(".playlist-list").animate({left:left},400);
+});
+
 })
